bddd43eec20120834038db4de0e0b5b5f5848baf
moreira <assembla@sicinfo.net.br>
2018-02-14 09:04:07 -0200

    salva

--- h-vue.js
+++ h-vue.js
@@ -3,50 +3,29 @@
  * 
  * dependency: systemjs
  * 
- * [0.3.0] inclui tags personalizada para vuejs
+ * [0.6.0] inclui tags personalizada para vuejs
  */
 
-((root, h) => {
+define([], () => function(cb) {
+  
+  const cx = this;
+  
+  return function(h) {
 
-  const isNode = 'undefined' !== typeof (process) && process.versions && process.versions.node
-  const isSystemjs = 'undefined' !== typeof (define);
-
-  if (isNode) module.exports = h(require('./hypertag'));
-
-  else if (isSystemjs) define(['hypertag'], hypertag => h(hypertag));
-
-})(this, Hypertag => {
-
-  class Tag extends Hypertag.Tag {
-
-    constructor(tagName, ...childs) {
-      if (Hypertag.existProps(childs, 'tagName')) {
-        tagName = childs[0].tagName;
-        delete childs[0].tagName;
-      }
-      super(tagName, ...childs)
-    }
-
-  }
-
-  class Input extends Hypertag.Tag {
+    return cb((a, ...c) => {
+      
+      let b = (
+        c.length &&
+        'object' == typeof (c[0]) &&
+        void 0 == c[0].tag &&
+        void 0 == c[0].data &&
+        void 0 == c[0].children
+      ) ? c.shift() : {};
+      
+      return h(a, b, c);
+      
+    }, cx);
     
-    constructor(model, props) {
-      super('input/', props);
-      this.setProps({'v-model': model, 'lazy': true});
-    }
-
-    change(arg) {
-      this.setProps({'@change': arg})
-      return this;
-    }
-
-  }
-
-  return {
-    Tag, Input,
-    'h': (...a) => new Tag(...a),
-    'input': (model, props) => new Input(model, props)
-  }
+  };
+  
 });
-

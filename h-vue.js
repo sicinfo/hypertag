66bd695a179c786f7a8474de92b193314a48459b
moreira <amoreirasilva@tjgo.jus.br>
2018-02-14 15:40:52 -0200

    atualiza

--- h-vue.js
+++ h-vue.js
@@ -6,26 +6,33 @@
  * [0.6.0] inclui tags personalizada para vuejs
  */
 
-define([], () => function(cb) {
-  
-  const cx = this;
-  
-  return function(h) {
+define([], () => {
 
-    return cb((a, ...c) => {
-      
-      let b = (
-        c.length &&
-        'object' == typeof (c[0]) &&
-        void 0 == c[0].tag &&
-        void 0 == c[0].data &&
-        void 0 == c[0].children
-      ) ? c.shift() : {};
-      
-      return h(a, b, c);
-      
-    }, cx);
-    
+  const version = '0.6.2'
+
+  const H = function (cb) {
+
+    return function (h, cx) {
+
+      console.log(cx);
+
+      return cb((a, ...c) => {
+
+        let b = (
+          c.length &&
+          'object' == typeof (c[0]) &&
+          void 0 == c[0].tag &&
+          void 0 == c[0].data &&
+          void 0 == c[0].children
+        ) ? c.shift() : {};
+
+        return h(a, b, c.length == 1 && Array.isArray(c[0]) ? c[0] : c);
+
+      }, cx || this);
+
+    };
   };
+
+  return { version, H }
   
 });

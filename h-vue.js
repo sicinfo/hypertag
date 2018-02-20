1b803d000c66b7f5bfb6425246e66c198bc2428c
moreira <assembla@sicinfo.net.br>
2018-02-19 22:55:42 -0300

    atualiza

--- h-vue.js
+++ h-vue.js
@@ -8,13 +8,43 @@
 
 define([], () => {
 
-  const version = '0.6.2'
+  const version = '0.7.0';
 
   const H = function (cb) {
 
     return function (h, cx) {
 
-      return cb((a, ...c) => {
+      const fe = {};
+
+      fe['text'] = (a, ...c) => {
+
+        let cx_name = c[0];
+        let b = c.length > 1 ? c[1] : {};
+
+        b['attrs'] || (
+          b['attrs'] = {}
+        );
+
+        b['attrs']['type'] || (
+          b['attrs']['type'] = 'text'
+        );
+
+        b['domProps'] || (
+          b['domProps'] = { 'value': cx_name }
+        );
+
+        b['on'] || (
+          b['on'] = {}
+        );
+
+        b['on']['change'] || (
+          b['on']['change'] = evt => { evt.stopPropagation(); cx_name = evt.target.value }
+        );
+
+        return h('input', b)
+      };
+
+      const fg = (a, ...c) => {
 
         let b = (
           c.length &&
@@ -24,11 +54,21 @@ define([], () => {
           void 0 == c[0].children
         ) ? c.shift() : {};
 
+        if ('on' in b) {
+          'click.prevent' in b.on && (
+            b.on['click'] = function(evt) { 
+              evt.stopPropagation(); 
+              b.on['click.prevent'].call(this, evt) 
+            }           
+          )
+        }
+
         c.length == 1 && Array.isArray(c[0]) && (c = c[0])
 
         return h(a, b, c);
+      };
 
-      }, cx || this);
+      return cb((a, ...c) => (fe[a] || fg)(a, ...c), cx || this);
 
     };
   };
